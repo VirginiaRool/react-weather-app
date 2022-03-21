f16173a368eee8a3ffb4f6cf70d81d019b31245f
VirginiaRool <virginia.aro@gmail.com>
2022-03-20 23:21:09 -0600

    Create component ActualDate

---
+++ src/ActualDate.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ActualDate(props) {
+  const date = props.date;
+  const options = {
+    weekday: "short",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return <div>{date.toLocaleDateString(undefined, options)}</div>;
+}
