b818f00132431c28a2ed9ff1ad696d0ea7277ca1
VirginiaRool <virginia.aro@gmail.com>
2022-03-20 23:20:38 -0600

    Created component WeatherDetails and WeatherSearch

---
+++ src/WeatherDetails.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./WeatherDetails.css";
+import Temperature from "./Temperature";
+import "bootstrap/dist/css/bootstrap.css";
+import ActualDate from "./ActualDate";
+
+export default function WeatherDetails(props) {
+  if (props.data === undefined) {
+    return null;
+  }
+  let city = props.data.name;
+  let icon = props.data.weather[0].icon;
+  let description = props.data.weather[0].description;
+  let temperature = Math.round(props.data.main.temp);
+  let humidity = props.data.main.humidity;
+  let wind = props.data.wind.speed;
+  let date = new Date(props.data.dt * 1000);
+
+  return (
+    <div className="weather-container">
+      <div className="app-container">
+        <div className="row">
+          <div className="col-6">
+            <div className="date">
+              <ul>
+                <li className="lastUpdated">
+                  <ActualDate date={date} />
+                </li>
+                <li className="city-name">{city}</li>
+                <li>
+                  <Temperature temperature={temperature} />
+                </li>
+                <li className="temp-description">{description}</li>
+                <li id="temperature" className="d-flex align-items-start"></li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="col-6">
+            <img
+              className="weather-image"
+              src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+              alt="icon"
+            />
+          </div>
+        </div>
+        <div>
+          <ul className="forecastDescription">
+            <li className="actualHumidity">Humidity: {humidity} %</li>
+            <li className="actualWind">Wind: {wind} km/hr</li>
+            <li className="showMaxTemp">Max: 25 °</li>
+            <li className="showMinTemp">Min: 14 °</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/WeatherSearch.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import WeatherDetails from "./WeatherDetails";
+import "./WeatherSearch.css";
+import background from "./images/background.jpg";
+
+import axios from "axios";
+
+export default function WeatherSearch() {
+  let apiKey = "78af43c4b86f6fe747e2eeb5803e94cd";
+  let units = "metric";
+
+  let [city, setCity] = useState("");
+  let [data, setData] = useState();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (city.length > 0) {
+      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+      axios.get(apiUrl).then(function (response) {
+        setData(response.data);
+      });
+      console.log(apiUrl);
+    } else {
+      alert(`Enter a city`);
+    }
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div>
+      <div
+        className="weather-search"
+        style={{ backgroundImage: `url(${background})` }}
+      >
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+                onChange={updateCity}
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" />
+            </div>
+          </div>
+        </form>
+        <WeatherDetails data={data} />
+      </div>
+      <p className="repository">
+        <a
+          href="https://github.com/VirginiaRool/react-weather-app "
+          target="_blank"
+          rel="noreferrer"
+          id="source-link"
+        >
+          Open-source code
+        </a>
+        <span> by Virginia Romero</span>
+      </p>
+    </div>
+  );
+}
