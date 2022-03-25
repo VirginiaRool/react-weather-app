453b9cca841731992cdbfe4360e9de344c220fea
VirginiaRool <virginia.aro@gmail.com>
2022-03-24 18:19:00 -0600

    Added new component

---
+++ src/WeatherForecast.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function WeatherForecast() {
+  return <div className="WeatherForecast">
+      <div className="row">
+          <div className="col-6"></div>
+      </div>
+  </div>;
+}
